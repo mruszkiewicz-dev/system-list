9255e513d1843b183d9777468f132acf69984de8
monkeymr014 <monkeymr014@gmail.com>
2023-01-17 13:29:06 +0100

    rename data

---
+++ src/data/index.js
@@ -0,0 +1,50 @@
+export const systemItem = [
+   {
+     id: 1,
+     title: 'EZD',
+     description: 'Elektroniczne zarządzanie dokumentami',
+     link: 'https://ezd.frse.org.pl/login',
+   },
+   {
+     id: 2,
+     title: 'System Kart Czasu Pracy',
+     description: 'Rozliczanie godzin pracy',
+     link: 'https://kcp.frse.org.pl',
+   },
+   {
+     id: 3,
+     title: 'HRcloud',
+     description: 'System kadrowy, szkolenia, wnioski urlopowe',
+     link: 'https://frse.symfoniahr.pl',
+   },
+   {
+     id: 4,
+     title: 'Magazyn',
+     description: 'Zgłoszenia IT, wnioski o sprzęt',
+     link: 'http://magazyn.frse.org.pl',
+   },
+   {
+     id: 5,
+     title: 'E-wnioski',
+     description: 'Nadawanie i odbieranie uprawnień do systemów',
+     link: 'https://e-wnioski.frse.org.pl',
+   },
+   {
+     id: 6,
+     title: 'Intranet',
+     description: 'Intranet FRSE',
+     link: 'https://intranet.frse.org.pl/',
+   },
+   {
+     id: 7,
+     title: 'Finera',
+     description: 'System dokumentów OnLine',
+     link: 'https://finera.frse.org.pl/',
+   },
+   {
+     id: 8,
+     title: 'Mantis',
+     description: 'System zgłoszeń dot. Finery, Forms oraz Narzędzi Komisyjnych',
+     link: 'http://mantis.frse.org.pl',
+   },
+ ];
--- src/templates/GridTemplate.js
+++ src/templates/GridTemplate.js
@@ -1,60 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
+import { systemItem } from '../data/index.js';
 import Card from '../components/Card/Card.js';
 import Arow from '../components/Arow/Arow.js';
-
-/* import Input from '../components/Input/Input.js';
- */
-const systemItem = [
-  {
-    id: 1,
-    title: 'EZD',
-    description: 'Elektroniczne zarządzanie dokumentami',
-    link: 'https://ezd.frse.org.pl/login',
-  },
-  {
-    id: 2,
-    title: 'System Rejestracji Czasu Pracy',
-    description: 'Rozliczanie godzin pracy',
-    link: 'https://kcp.frse.org.pl',
-  },
-  {
-    id: 3,
-    title: 'HRcloud',
-    description: 'System kadrowy, szkolenia, wnioski urlopowe',
-    link: 'https://frse.symfoniahr.pl',
-  },
-  {
-    id: 4,
-    title: 'Magazyn',
-    description: 'Zgłoszenia IT, wnioski o sprzęt',
-    link: 'http://magazyn.frse.org.pl',
-  },
-  {
-    id: 5,
-    title: 'E-wnioski',
-    description: 'Nadawanie i odbieranie uprawnień do systemów',
-    link: 'https://e-wnioski.frse.org.pl',
-  },
-  {
-    id: 6,
-    title: 'Intranet',
-    description: 'Intranet FRSE',
-    link: 'https://intranet.frse.org.pl/',
-  },
-  {
-    id: 7,
-    title: 'Finera',
-    description: 'System dokumentów OnLine',
-    link: 'https://finera.frse.org.pl/',
-  },
-  {
-    id: 8,
-    title: 'Mantis',
-    description: 'System zgłoszeń dot. Finery, Forms oraz Narzędzi Komisyjnych',
-    link: 'http://mantis.frse.org.pl',
-  },
-];
 
 const StyledWrapper = styled.div`
   position: relative;
@@ -110,29 +58,28 @@ const H5 = styled.h5`
 const StyleFAQ = styled.div`
   font-size: 18px;
   text-decoration: none;
-  & a{
+  & a {
     text-decoration: none;
     color: black;
     background-color: #054371;
-  color: white;
-  border-radius: 6px;
-  position: absolute;
-  right: 30px;
-  top: 30px;
-  padding: 14px 25px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  -webkit-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
-  -moz-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
-  box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
-  &:hover {
-    transform: scale(1.1);
-    transition: 0.3s ease-out;
+    color: white;
+    border-radius: 6px;
+    position: absolute;
+    right: 30px;
+    top: 30px;
+    padding: 14px 25px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    -webkit-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+    -moz-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+    box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+    &:hover {
+      transform: scale(1.1);
+      transition: 0.3s ease-out;
+    }
   }
-
-  } 
-`
+`;
 class GridTemplate extends React.Component {
   state = {
     showMore: false,
